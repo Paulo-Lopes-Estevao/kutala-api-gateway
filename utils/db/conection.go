376b48a0262c3ec7f3875a795e34c2a5e87699b1
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-09 16:08:31 +0100

    add: connection postgres

---
+++ utils/db/conection.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const DB_USERNAME = "snirdb"
+const DB_PASSWORD = "snirdb@2021"
+const DB_NAME = "nzimbupaygateway"
+const DB_HOST = "172.16.16.36"
+const DB_PORT = "5432"
+
+func ConnectionDB() *gorm.DB {
+
+	dsn := "host=" + DB_HOST + " user=" + DB_USERNAME + " password=" + DB_PASSWORD + " dbname=" + DB_NAME + " port=" + DB_PORT + " sslmode=disable"
+
+	db, err := gorm.Open("postgres", dsn)
+
+	if err != nil {
+		defer db.Close()
+		panic("failed to connect database")
+
+	}
+
+	return db
+
+}
