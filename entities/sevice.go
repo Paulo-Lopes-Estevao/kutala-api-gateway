9542ed14363271697af29e6a476720b7cb1b2848
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-09 22:15:18 +0100

    update: entities service

--- entities/sevice.go
+++ entities/sevice.go
@@ -4,7 +4,7 @@ import "time"
 
 type Service struct {
 	Path            string `json:"path"`
-	Id_Microservice Microservice
+	Id_microservice Microservice
 	Header          string    `json:"header"`
 	Method          string    `json:"Method"`
 	State           bool      `json:"state"`
