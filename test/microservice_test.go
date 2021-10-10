a301d31e25598d5759417fc5c72315ca88a0659e
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-10 01:11:58 +0100

    rename: entities for domain

--- test/microservice_test.go
+++ test/microservice_test.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/entities"
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/domain"
 	"github.com/bxcodec/faker/v3"
 )
 
 func TestNewMicroservice(t *testing.T) {
-	m_service := entities.NewMicroservice(faker.Name(), "http", "172.0.0.1", "demo", "demo")
+	m_service := domain.NewMicroservice(faker.Name(), "http", "172.0.0.1", "demo", "demo")
 	fmt.Println(m_service)
 }
