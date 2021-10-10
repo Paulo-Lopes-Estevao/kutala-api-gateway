9a621456e0b58b6fa740203ab9eba1233260efd7
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-10 02:17:13 +0100

    test: repository insert microservice

--- test/microservice_test.go
+++ test/microservice_test.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/app/repository"
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/domain"
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/database"
 	"github.com/bxcodec/faker/v3"
 )
 
 func TestNewMicroservice(t *testing.T) {
 	m_service := domain.NewMicroservice(faker.Name(), "http", "172.0.0.1", "demo", "demo")
 	fmt.Println(m_service)
+}
+
+func TestInsertMicroservice(t *testing.T) {
+	conectdb := database.ConnectionDB()
+	defer conectdb.Close()
+
+	repository := repository.MicroserviceRepository{Db: conectdb}
+
+	m_service := domain.NewMicroservice(faker.Name(), "http", "172.0.0.1", "demo", "demo")
+
+	repository.InsertMicroservice(m_service)
+
 }
