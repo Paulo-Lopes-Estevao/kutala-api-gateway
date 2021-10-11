be9b941a80ae0e640f896c8fce5599ce4540a2a1
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-11 19:04:50 +0100

    add: Auth

--- app/usecase/microservice_usecase.go
+++ app/usecase/microservice_usecase.go
@@ -1,12 +1,15 @@
 package usecase
 
 import (
+	"fmt"
+
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/app/repository"
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/domain/entities"
 )
 
 type MicroserviceUseCaseInterface interface {
 	CreateMicroService(microservice *entities.Microservice) (*entities.Microservice, error)
+	Auth(username string, password string, microservice *entities.Microservice) (*entities.Microservice, error)
 }
 
 type microserviceUseCase struct {
@@ -19,7 +22,7 @@ func NewMicroserviceUseCase(repository repository.MicroserviceRepositoryInterfac
 
 func (usecase *microserviceUseCase) CreateMicroService(microservice *entities.Microservice) (*entities.Microservice, error) {
 
-	data := entities.NewMicroservice(microservice.Name, microservice.Username, microservice.Password)
+	data, _ := entities.NewMicroservice(microservice.Name, microservice.Username, microservice.Password)
 
 	microservices, err := usecase.microserviceRepository.InsertMicroservice(data)
 
@@ -28,4 +31,18 @@ func (usecase *microserviceUseCase) CreateMicroService(microservice *entities.Mi
 	}
 
 	return microservices, nil
+}
+
+func (usecase *microserviceUseCase) Auth(username string, password string, microservice *entities.Microservice) (*entities.Microservice, error) {
+	data, err := usecase.microserviceRepository.FindUsernameMicroservice(username, microservice)
+
+	if err != nil {
+		return nil, fmt.Errorf("The password is invalid for the username")
+	}
+
+	if data.VerifyPassword(password) {
+		return data, nil
+	}
+
+	return nil, fmt.Errorf("The password is invalid for the username")
 }
