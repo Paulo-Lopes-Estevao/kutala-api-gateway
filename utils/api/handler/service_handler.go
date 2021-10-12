71b3b69e522ac12f53b397ac24dd864d6118ccb1
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-12 23:24:51 +0100

    add: handle service

---
+++ utils/api/handler/service_handler.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/controller"
+	"github.com/labstack/echo"
+)
+
+func PrivateRouteServiceHandler(e *echo.Echo, c controller.AppController) *echo.Echo
+
+func PublicRouteServiceHandler(e *echo.Echo, c controller.AppController) *echo.Echo {
+
+	e.POST("/service", func(context echo.Context) error { return c.Service.AddService(context) })
+	e.GET("/service/:id", func(context echo.Context) error { return c.Service.FindByUuidService(context) })
+
+	return e
+
+}
