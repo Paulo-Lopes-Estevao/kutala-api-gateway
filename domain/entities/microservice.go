fa4ae5805d366bf3564afbd7ccf2761a0133e29a
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-12 23:26:09 +0100

    edit: entities microservice

--- domain/entities/microservice.go
+++ domain/entities/microservice.go
@@ -4,60 +4,43 @@ import (
 	"time"
 
 	"github.com/asaskevich/govalidator"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type Microservice struct {
-	Id       string    `json:"id"`
-	Name     string    `json:"name"`
-	Username string    `json:"username"`
-	Password string    `json:"password"`
-	State    bool      `json:"state"`
-	Created  time.Time `json:"created"`
-	Updated  time.Time `json:"updated"`
+	Id              string    `json:"id"`
+	Scheme          string    `json:"scheme"`
+	Host            string    `json:"host"`
+	Path            string    `json:"path"`
+	Header          string    `json:"header"`
+	Method          string    `json:"method"`
+	Id_microservice Service   `json:"id_microservice"`
+	State           bool      `json:"state"`
+	Created         time.Time `json:"created"`
+	Updated         time.Time `json:"updated"`
 }
 
-func NewMicroservice(name string, username string, password string) (*Microservice, error) {
+func NewMicroservice(scheme string, host string, path string, header string, method string, id_microservice Service) (*Microservice, error) {
+
 	microservice := &Microservice{
-		Name:     name,
-		Username: username,
-		Password: password,
-		State:    true,
-		Created:  time.Now(),
-		Updated:  time.Now(),
+		Scheme:          scheme,
+		Host:            host,
+		Path:            path,
+		Header:          header,
+		Method:          method,
+		Id_microservice: id_microservice,
+		State:           true,
+		Created:         time.Now(),
+		Updated:         time.Now(),
 	}
 
-	err := microservice.passwordEncrypt()
+	err := microservice.validate()
 
 	if err != nil {
 		return nil, err
 	}
 
 	return microservice, nil
-}
 
-func (microservice *Microservice) passwordEncrypt() error {
-	password, err := bcrypt.GenerateFromPassword([]byte(microservice.Password), bcrypt.DefaultCost)
-
-	if err != nil {
-		return err
-	}
-
-	microservice.Password = string(password)
-
-	err = microservice.validate()
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
-func (microservice *Microservice) VerifyPassword(password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(microservice.Password), []byte(password))
-	return err == nil
 }
 
 func (microservice *Microservice) validate() error {
