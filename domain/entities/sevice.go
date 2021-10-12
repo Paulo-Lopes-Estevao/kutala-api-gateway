2cef6e4a629077112c63eaf77449f80fdcfc839f
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-12 06:02:05 +0100

    add: newService

--- domain/entities/sevice.go
+++ domain/entities/sevice.go
@@ -1,6 +1,10 @@
 package entities
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
 
 type Service struct {
 	Id              string `json:"id"`
@@ -8,9 +12,44 @@ type Service struct {
 	Host            string `json:"host"`
 	Path            string `json:"path"`
 	Header          string `json:"header"`
-	Method          string `json:"Method"`
+	Method          string `json:"method"`
 	Id_microservice Microservice
-	State           bool   `json:"state"`
+	State           bool      `json:"state"`
 	Created         time.Time `json:"created"`
 	Updated         time.Time `json:"updated"`
+}
+
+func NewService(scheme string, host string, path string, header string, method string, id_microservice Microservice) (*Service, error) {
+
+	service := &Service{
+		Scheme:          scheme,
+		Host:            host,
+		Path:            path,
+		Header:          header,
+		Method:          method,
+		Id_microservice: id_microservice,
+		State:           true,
+		Created:         time.Now(),
+		Updated:         time.Now(),
+	}
+
+	err := service.validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return service, nil
+
+}
+
+func (service *Service) validate() error {
+
+	_, err := govalidator.ValidateStruct(service)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
