45d0fc44c08c130825c49ae736b3663319a90bfc
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-13 18:35:17 +0100

    add: context Request

--- controller/context.go
+++ controller/context.go
@@ -1,9 +1,12 @@
 package controller
 
+import "net/http"
+
 type ResponseData map[string]interface{}
 
 type Context interface {
 	JSON(code int, i interface{}) error
 	Bind(i interface{}) error
 	Param(name string) string
+	Request() *http.Request
 }
