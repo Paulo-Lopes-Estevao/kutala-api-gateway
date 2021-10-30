4126d89426b1d100aae8ad9329563e3e748ac314
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-31 00:59:25 +0100

    edit: modify frame echo for gorilla mux

--- utils/api/handler/microservice_handler.go
+++ utils/api/handler/microservice_handler.go
@@ -3,14 +3,14 @@ package handler
 import (
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/controller"
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/proxy"
-	"github.com/labstack/echo"
+	"github.com/gorilla/mux"
 )
 
-func RouteMicroserviceHandler(e *echo.Echo, c controller.AppController) *echo.Echo {
+func RouteMicroserviceHandler(e *mux.Router, c controller.AppController) *mux.Router {
 
-	e.POST("/microservice", func(context echo.Context) error { return c.Microservice.AddMicroservice(context) })
-	e.GET("/v1/africa", echo.WrapHandler(proxy.ReverseProxy()))
-	//e.Any("/:pathmicroservice", echo.WrapHandler()))
+	e.HandleFunc("/microservice", c.Microservice.AddMicroservice).Methods("POST")
+	e.HandleFunc("/graphql", proxy.Handler(proxy.ReverseProxy())).Methods("GET")
+
 	return e
 
 }
