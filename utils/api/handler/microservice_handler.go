855f4fd71b60e6c6cae476ae287be9a14d9cb124
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-11-15 09:23:46 +0100

    edit: add route ReverseProxy

--- utils/api/handler/microservice_handler.go
+++ utils/api/handler/microservice_handler.go
@@ -2,12 +2,17 @@ package handler
 
 import (
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/interface/controller"
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/middleware"
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/proxy"
 	"github.com/labstack/echo"
 )
 
 func RouteMicroserviceHandler(e *echo.Echo, c controller.AppController) *echo.Echo {
 
-	e.POST("/microservice/:id", func(context echo.Context) error { return c.Microservice.AddMicroservice(context) })
+	e.POST("/microservice", func(context echo.Context) error { return c.Microservice.AddMicroservice(context) })
+	e.POST("/microservice/:id/:name", func(context echo.Context) error { return proxy.ReverseProxy(context, c) })
+
+	middleware.RegistryMiddlewareBasicAuth(e, c)
 
 	return e
 
