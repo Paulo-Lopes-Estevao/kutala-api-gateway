727f229981198ff2a3c82d87f628dad61b5a14c4
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-21 17:27:57 +0100

    edit: update server

--- main.go
+++ main.go
@@ -3,48 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/injection"
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/handler"
 
-	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/middleware"
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/database"
 	"github.com/labstack/echo"
-	echo_middleware "github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/middleware"
 )
 
-var public_server = echo.New()
-
-var private_server = echo.New()
+var server = echo.New()
 
 func main() {
 
 	db := database.ConnectionDB()
 	i := injection.NewRegistry(db)
 
-	public_server.Use(echo_middleware.Logger())
-	public_server.Use(echo_middleware.Recover())
+	server.Use(middleware.Logger())
+	server.Use(middleware.Recover())
 
-	middleware.RegistryMiddlewareBasicAuth(private_server, i.NewAppController())
+	handler.RouteMicroserviceHandler(server, i.NewAppController())
 
-	handler.RouteMicroserviceHandler(private_server, i.NewAppController())
+	handler.RouteUserHandler(server, i.NewAppController())
 
-	handler.RouteUserHandler(public_server, i.NewAppController())
-
-	go func() {
-		fmt.Println("Running Server A")
-		fmt.Println("Server started at port 8080")
-		if err := private_server.Start(":8080"); err != http.ErrServerClosed {
-			log.Println("Not Running Server A...", err.Error())
-		}
-	}()
-
-	fmt.Println("Running Server B")
-	fmt.Println("Server started at port 8081")
-	err := public_server.Start(":8081")
+	fmt.Println("Server started at port 9000")
+	err := server.Start(":9000")
 
 	if err != nil {
-		log.Println("Not Running Server B...", err.Error())
+		log.Println("Not Running Server ...", err.Error())
 	}
 }
