303a0c0b32a0ded74bd7563a6b86ec8d25d9a139
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-22 07:17:48 +0100

    update: prensent fields

--- interface/presenter/response_presenter.go
+++ interface/presenter/response_presenter.go
@@ -5,14 +5,15 @@ import "github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/domain/entities"
 func Response(userservice *entities.Microservice, User *entities.User) *entities.Microservice {
 
 	return &entities.Microservice{
-		Id:      userservice.Id,
-		Name:    userservice.Name,
-		Version: userservice.Version,
-		Api:     userservice.Api,
-		Path:    userservice.Path,
-		Method:  userservice.Method,
-		Iduser:  User.Id,
-		User:    User,
+		Id:       userservice.Id,
+		Name:     userservice.Name,
+		Version:  userservice.Version,
+		Api:      userservice.Api,
+		Endpoint: userservice.Endpoint,
+		Type:     userservice.Type,
+		Method:   userservice.Method,
+		Iduser:   User.Id,
+		User:     User,
 	}
 
 }
