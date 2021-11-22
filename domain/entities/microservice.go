2fe2d630caffd78106aaacfb80bc6c4c67f36d6f
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-22 07:17:11 +0100

    edit: add fields

--- domain/entities/microservice.go
+++ domain/entities/microservice.go
@@ -1,37 +1,42 @@
 package entities
 
 import (
+	"strings"
 	"time"
 
 	"github.com/asaskevich/govalidator"
 )
 
 type Microservice struct {
-	Id      string    `json:"id"`
-	Name    string    `json:"name"`
-	Version string    `json:"version"`
-	Api     string    `json:"api"`
-	Path    string    `json:"path"`
-	Method  string    `json:"method"`
-	Iduser  string    `json:"iduser"`
-	User    *User     `json:"user"`
-	State   bool      `json:"state"`
-	Created time.Time `json:"created"`
-	Updated time.Time `json:"updated"`
+	Id       string    `json:"id"`
+	Name     string    `json:"name"`
+	Version  string    `json:"version"`
+	Api      string    `json:"api"`
+	Endpoint string    `json:"endpoint"`
+	Type     string    `json:"type"`
+	Method   string    `json:"method"`
+	Iduser   string    `json:"iduser"`
+	User     *User     `json:"user"`
+	State    bool      `json:"state"`
+	Created  time.Time `json:"created"`
+	Updated  time.Time `json:"updated"`
 }
 
-func NewMicroservice(name string, version string, api string, path string, method string, iduser string) (*Microservice, error) {
+func NewMicroservice(name string, version string, api string, endpoint string, typer string, method string, iduser string) (*Microservice, error) {
+
+	typerUpper := upperText(typer)
 
 	microservice := &Microservice{
-		Name:    name,
-		Version: version,
-		Api:     api,
-		Path:    path,
-		Method:  method,
-		Iduser:  iduser,
-		State:   true,
-		Created: time.Now(),
-		Updated: time.Now(),
+		Name:     name,
+		Version:  version,
+		Api:      api,
+		Type:     typerUpper,
+		Endpoint: endpoint,
+		Method:   method,
+		Iduser:   iduser,
+		State:    true,
+		Created:  time.Now(),
+		Updated:  time.Now(),
 	}
 
 	err := microservice.validate()
@@ -41,6 +46,25 @@ func NewMicroservice(name string, version string, api string, path string, metho
 	}
 
 	return microservice, nil
+
+}
+
+func upperText(text string) string {
+	return strings.ToUpper(text)
+}
+
+func (microservice *Microservice) VerifyTypeProtocolComunication(protocol string) bool {
+	typer := []string{"GRAPHQL", "REST"}
+
+	protocolUpper := upperText(protocol)
+
+	for _, v := range typer {
+		if protocolUpper == v {
+			return true
+		}
+	}
+
+	return false
 
 }
 
