7fc5c93effdaeadcb7bfddeb242316875adf6bb3
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-12-17 01:11:21 +0100

    feat: edit prometheus

--- main.go
+++ main.go
@@ -5,31 +5,33 @@ import (
 	"log"
 
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/injection"
-	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/handler"
 
+	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/api/handler"
 	"github.com/Paulo-Lopes-Estevao/NZIMBUPAY-api-gateway/utils/database"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
 )
 
-var server = echo.New()
-
 func main() {
+
+	server := echo.New()
 
 	db := database.ConnectionDB()
 	i := injection.NewRegistry(db)
 
-	server.Use(middleware.Logger())
-	server.Use(middleware.Recover())
+	//server.Use(middleware.Logger())
+	//server.Use(middleware.Recover())
+
+	p := prometheus.NewPrometheus("echo", nil)
 
 	handler.RouteMicroserviceHandler(server, i.NewAppController())
 
 	handler.RouteUserHandler(server, i.NewAppController())
 
-	fmt.Println("Server started at port 9000")
-	err := server.Start(":9000")
+	p.Use(server)
 
-	if err != nil {
-		log.Println("Not Running Server ...", err.Error())
+	fmt.Println("Server started at port 9000")
+	if err := server.Start(":9000"); err != nil {
+		log.Println("Not Running Server A...", err.Error())
 	}
 }
