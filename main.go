d8e3f9970ba8d415123529097a70e46c9022ce42
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-04 00:31:52 +0100

    feat: entry middlewera

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Paulo-Lopes-Estevao/kutala-api-gateway/utils/database"
 	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -19,8 +20,8 @@ func main() {
 	db := database.ConnectionDB()
 	i := injection.NewRegistry(db)
 
-	//server.Use(middleware.Logger())
-	//server.Use(middleware.Recover())
+	server.Use(middleware.Logger())
+	server.Use(middleware.Recover())
 
 	p := prometheus.NewPrometheus("echo", nil)
 
@@ -29,6 +30,32 @@ func main() {
 	handler.RouteUserHandler(server, i.NewAppController())
 
 	p.Use(server)
+
+	/* 	signingKey := []byte("secret")
+
+	   	config := middleware.JWTConfig{
+	   		TokenLookup: "query:token",
+	   		ParseTokenFunc: func(auth string, c echo.Context) (interface{}, error) {
+	   			keyFunc := func(t *jwt.Token) (interface{}, error) {
+	   				if t.Method.Alg() != "HS256" {
+	   					return nil, fmt.Errorf("unexpected jwt signing method=%v", t.Header["alg"])
+	   				}
+	   				return signingKey, nil
+	   			}
+
+	   			// claims are of type `jwt.MapClaims` when token is created with `jwt.Parse`
+	   			token, err := jwt.Parse(auth, keyFunc)
+	   			if err != nil {
+	   				return nil, err
+	   			}
+	   			if !token.Valid {
+	   				return nil, errors.New("invalid token")
+	   			}
+	   			return token, nil
+	   		},
+	   	}
+
+	   	server.Use(middleware.JWTWithConfig(config)) */
 
 	fmt.Println("Server started at port 9000")
 	if err := server.Start(":9000"); err != nil {
