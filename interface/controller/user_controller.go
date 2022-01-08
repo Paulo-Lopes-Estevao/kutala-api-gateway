c163dc4eadc52ef429a3980e2836c3fc2b7b0efd
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-08 14:38:30 +0100

    feat: add controller AuthJWTUser

--- interface/controller/user_controller.go
+++ interface/controller/user_controller.go
@@ -2,15 +2,19 @@ package controller
 
 import (
 	"errors"
+	"github.com/Paulo-Lopes-Estevao/kutala-api-gateway/bootstrap"
 	"net/http"
+	"time"
 
 	"github.com/Paulo-Lopes-Estevao/kutala-api-gateway/domain/entities"
 	"github.com/Paulo-Lopes-Estevao/kutala-api-gateway/usecase/interactor"
+	"github.com/golang-jwt/jwt"
 )
 
 type UserControllerInterface interface {
 	AddUser(ctx Context) error
 	AuthBasicUser(username, password string, ctx Context) (*entities.User, error)
+	AuthJWTUser(ctx Context) error
 	FindByUuidUser(ctx Context) error
 }
 
@@ -48,6 +52,47 @@ func (usecasecontroller *userController) AuthBasicUser(username, password string
 	}
 
 	return services, nil
+}
+
+type jwtCustomClaims struct {
+	IdUser     string `json:"id_user"`
+	Authorized bool   `json:"authorized"`
+	jwt.StandardClaims
+}
+
+func (usecasecontroller *userController) AuthJWTUser(ctx Context) error {
+
+	signingKey := []byte(bootstrap.GoDotEnvVariable("JWT_SECRET_KEY"))
+
+	if err := ctx.Bind(&user); !errors.Is(err, nil) {
+		ctx.JSON(http.StatusBadRequest, ResponseData{"error": err.Error()})
+	}
+
+	services, err := usecasecontroller.userUseCase.Auth(user.Username, user.Password, &user)
+
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, ResponseData{"error": err.Error()})
+	}
+
+	claims := &jwtCustomClaims{
+		services.Id,
+		true,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString(signingKey)
+
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, ResponseData{"error": err.Error()})
+	}
+
+	return ctx.JSON(http.StatusOK, ResponseData{
+		"token": t,
+	})
 }
 
 func (usecasecontroller *userController) FindByUuidUser(ctx Context) error {
